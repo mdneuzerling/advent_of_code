c7117ade91ac3c1837cfd11a7be4651dd4c20caf
mdneuzerling <david@neuzerling.com>
2021-12-07 09:12:51 +1100

    Use matrix-based approach for day 6

--- AOC2021/src/day06.jl
+++ AOC2021/src/day06.jl
@@ -5,44 +5,47 @@ input = read(joinpath(data_dir, "day06"), String)
 
 export part1, part2
 
-function count_fish_days_to_spawn(input::String)
+"""
+Converts a string like "0,1,1,1,2,2" into a vector
+[1
+ 3
+ 2]
+Note the change in 0-based indexing to 1-based indexing.
+"""
+function collect_days_to_spawn(input::String)
     days_to_spawn = [parse(Int8, string(day)) for day in split(input, ",")]
-    Dict(day => count(x -> x == day, days_to_spawn) for day in unique(days_to_spawn))
+    # the below line converts the 0-based indexing of the input to 1-based indexing
+    [count(x -> x == (day - 1), days_to_spawn) for day = 1:size(age_matrix)[1]]
 end
 
-function age(fish_days_to_spawn::Dict{Int8}{Int64})
-    new_counts = Dict{Int8}{Int64}()
-    # resisting the urge to use modular arithmetic. 0 becomes 6, but 8 becomes 7
-    for days_to_spawn = 1:8
-        next_days_to_spawn = days_to_spawn - 1
-        new_counts[next_days_to_spawn] = get(fish_days_to_spawn, days_to_spawn, 0)
-    end
-    new_counts[6] = new_counts[6] + get(fish_days_to_spawn, 0, 0)
-    new_counts[8] = get(fish_days_to_spawn, 0, 0) # new babies
-    new_counts
+const age_matrix = [
+    0 1 0 0 0 0 0 0 0
+    0 0 1 0 0 0 0 0 0
+    0 0 0 1 0 0 0 0 0
+    0 0 0 0 1 0 0 0 0
+    0 0 0 0 0 1 0 0 0
+    0 0 0 0 0 0 1 0 0
+    1 0 0 0 0 0 0 1 0
+    0 0 0 0 0 0 0 0 1
+    1 0 0 0 0 0 0 0 0]
+
+function age(days_to_spawn::Vector{Int64}, days::Int64 = 1)
+    age_matrix^days * days_to_spawn
 end
 
-function age(fish_days_to_spawn::Dict{Int8}{Int64}, days::Int64)
-    working_copy = copy(fish_days_to_spawn)
-    for i = 1:days
-        working_copy = age(working_copy)
-    end
-    working_copy
-end
-
-function count_after_aging(fish_days_to_spawn::Dict{Int8}{Int64}, days::Int64)
-    aged = age(fish_days_to_spawn, days)
+function count_after_aging(days_to_spawn::Vector{Int64}, days::Int64)
+    aged = age(days_to_spawn, days)
     sum(values(aged))
 end
 
 function part1(; input = input)
-    fish_days_to_spawn = count_fish_days_to_spawn(input)
-    count_after_aging(fish_days_to_spawn, 80)
+    days_to_spawn = collect_days_to_spawn(input)
+    count_after_aging(days_to_spawn, 80)
 end
 
 function part2(; input = input)
-    fish_days_to_spawn = count_fish_days_to_spawn(input)
-    count_after_aging(fish_days_to_spawn, 256)
+    days_to_spawn = collect_days_to_spawn(input)
+    count_after_aging(days_to_spawn, 256)
 end
 
 end # module
