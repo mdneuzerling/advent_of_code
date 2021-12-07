0fde5974b09614f090cc17f59a1b746ca0d867bd
mdneuzerling <david@neuzerling.com>
2021-12-07 16:43:22 +1100

    day07

---
+++ AOC2021/src/day07.jl
@@ -0,0 +1,61 @@
+module Day07
+
+using Statistics
+
+import ..data_dir # from parent module
+input = read(joinpath(data_dir, "day07"), String)
+
+export part1, part2
+
+"""
+The total fuel required to align all crab submarines to `target`
+This function accepts an optional `fuel_cost_function` which calculates the fuel
+required to move a crab submarine from one location to another. This function 
+should act on non-negative integer values (ie. absolute difference). The default
+is the `identity` function, in which the distance between the two locations is
+also the fuel cost.
+"""
+function fuel_to_align(
+    positions::Vector{Int64},
+    target::Integer,
+    fuel_cost_function::Function = identity
+)
+    absolute_movements = abs.(positions .- target)
+    fuel_costs = fuel_cost_function.(absolute_movements)
+    sum(fuel_costs)
+end
+
+"""
+I'm calling this `brute_force` since it inelegantly searches the entire solution
+space for a minimum cost. If this function isn't good enough for part 2 I can
+look for leaner search strategies (starting from median position maybe?).
+
+Starting with the assumption that the optimal position must be somewhere between
+the minimum and maximum positions, we can check each possible position between 
+the two to determine the minimum fuel consumption.
+"""
+function brute_force(positions::Vector{Int64}, fuel_cost_function::Function = identity)
+    minimum_fuel = Inf
+    for position = minimum(positions):maximum(positions)
+        required_fuel = fuel_to_align(positions, position, fuel_cost_function)
+        minimum_fuel = min(required_fuel, minimum_fuel)
+    end
+    Integer(minimum_fuel)
+end
+
+function part1(; input = input)
+    positions = [parse(Int64, position) for position in split(input, ",")]
+    brute_force(positions)
+end
+
+"""
+I think this is an appropriate name? We're summing an arithmetic sequence.
+"""
+arithmetic_fuel_cost(movement::Int64) = sum(1:movement)
+
+function part2(; input = input)
+    positions = [parse(Int64, position) for position in split(input, ",")]
+    brute_force(positions, arithmetic_fuel_cost)
+end
+
+end # module
---
+++ AOC2021/test/day07.jl
@@ -0,0 +1,20 @@
+module TestDay07
+
+import AOC2021.Day07.part1, AOC2021.Day07.part2
+using Test
+
+test_input_raw = """
+16,1,2,0,4,2,7,1,2,14"""
+
+test_input = test_input_raw # no transformation needed here
+
+@testset "Day 07" begin
+    @testset "part 1" begin
+        @test part1(; input = test_input) == 37
+    end
+    @testset "part 2" begin
+        @test part2(; input = test_input) == 168
+    end
+end
+
+end # module
